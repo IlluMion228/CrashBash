e6768d0014f9b2f1070cc2b1b4024f57e03e4a55
Роман Пеев <romanpeev4@gmail.com>
2024-12-13 21:24:24 +0200

    Added GameViewModel, changes in MainPage.xaml

--- CrashBash/Models/Gameboard.cs
+++ CrashBash/Models/Gameboard.cs
@@ -1,38 +1,40 @@
 ﻿
 
-namespace CrashBash.Models;
+namespace CrashBash.Models
+{
 
     public class Gameboard
     {
         public int Rows { get; }
         public int Columns { get; }
-        public List <Cell> Cells { get; private set; }
+        public List<Cell> Cells { get; private set; }
 
-        public Gameboard(int rows, int columns) 
-        { 
-            Rows= rows; 
-            Columns=columns;
+        public Gameboard(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
             Cells = new List<Cell>();
-            InitializeBoard ();
+            InitializeBoard();
         }
-        private void InitializeBoard() 
+        private void InitializeBoard()
         {
             var candyTypes = new[] { "Red", "Blue", "Green", "Yellow", "Purple" };
             var random = new Random();
 
-            for (int row = 0; row < Rows; row++) 
-            { 
+            for (int row = 0; row < Rows; row++)
+            {
                 for (int col = 0; col < Columns; col++)
                 {
                     Cells.Add(new Cell
                     {
-                        Row= row,
-                        Column= col,
-                        CandyType= candyTypes[random.Next(candyTypes.Length)]
-                    });                
+                        Row = row,
+                        Column = col,
+                        CandyType = candyTypes[random.Next(candyTypes.Length)]
+                    });
                 }
             }
 
         }
     }
 
+}
---
+++ CrashBash/ViewModels/GameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CrashBash.Models;
+using System.Collections.ObjectModel;
+
+namespace CrashBash.ViewModels;
+
+
+    public class GameViewModel
+    {
+        public ObservableCollection<Models.Cell> Cells { get; set; }
+        public GameViewModel()
+        {
+            var gameBoard = new Gameboard(8, 8);
+            Cells = new ObservableCollection<Models.Cell>(gameBoard.Cells);
+        }
+    };
+
