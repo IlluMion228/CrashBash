99b83ac48b872b86445fa056e78a35a89a3a5646
Роман Пеев <romanpeev4@gmail.com>
2024-12-13 21:10:15 +0200

    Added 2 models for game Cell, Gameboard. Next step ViewModels

---
+++ CrashBash/Models/Cell.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CrashBash.Models
+{
+    public class Cell
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public string CandyType { get; set; }
+        public bool isSelected { get; set; }
+    }
+}
---
+++ CrashBash/Models/Gameboard.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace CrashBash.Models;
+
+    public class Gameboard
+    {
+        public int Rows { get; }
+        public int Columns { get; }
+        public List <Cell> Cells { get; private set; }
+
+        public Gameboard(int rows, int columns) 
+        { 
+            Rows= rows; 
+            Columns=columns;
+            Cells = new List<Cell>();
+            InitializeBoard ();
+        }
+        private void InitializeBoard() 
+        {
+            var candyTypes = new[] { "Red", "Blue", "Green", "Yellow", "Purple" };
+            var random = new Random();
+
+            for (int row = 0; row < Rows; row++) 
+            { 
+                for (int col = 0; col < Columns; col++)
+                {
+                    Cells.Add(new Cell
+                    {
+                        Row= row,
+                        Column= col,
+                        CandyType= candyTypes[random.Next(candyTypes.Length)]
+                    });                
+                }
+            }
+
+        }
+    }
+
